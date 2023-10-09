2dd4226090cf3a554eeccd9973763f767f79e956
rsfunc <h.vd.esker+rs@gmail.com>
2023-10-09 20:33:56 +0200

    change startup

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,11 @@
+use actix_web::HttpServer;
 use p_web::server;
 
-fn configurator(config: &mut actix_web::web::ServiceConfig) {
-    crate::routes::user_config(config)
-}
-
 pub async fn run() -> std::io::Result<()> {
-    server::run(server::SessionStore::Cookie, configurator).await
+    HttpServer::new(move || {
+        server::app(server::StorageType::Cookie).configure(crate::routes::user_config)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
